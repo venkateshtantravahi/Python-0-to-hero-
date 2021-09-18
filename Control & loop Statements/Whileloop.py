4e032e3e83552e18413e7bcd4d6c2f5ce02e8744
venkatesh <64308188+venkateshtantravahi@users.noreply.github.com>
2021-09-18 23:38:01 +0530

    Add files via upload

---
+++ Control & loop Statements/Whileloop.py
@@ -0,0 +1,28 @@
+"""
+The while loop in python is used to iterate over a block of code 
+as long as the test expression is true.We generally use this when we don't know the number of 
+iterations beforehand.
+
+While test_expression:
+    Body of while loop
+
+Python interprets any non-zero numbers as True. None and '0' are interpreted as false
+"""
+#program to sum n digits
+n = int(input("Enter a number: "))
+#intializing sum and counter 
+sum = 0
+counter = 1
+
+while counter<=n:
+    sum += counter
+    counter += 1 #updating a counter
+    print("Counter is: ", counter)
+
+print("The sum of " + str(n) + " numbers is ", sum)
+
+#loops are always infinite 
+while counter == n:
+    print("Equals")
+else:
+    print("Done")
---
+++ comprehensions/Dict_Comprehension.py
@@ -0,0 +1,39 @@
+"""
+Dictionary comprehension is a simpler or concise way of declaring dictionary 
+
+dictionary = {key: value for var in iterable}
+"""
+old_price = {'milk': 1.02, 'coffee': 2.5, 'butter':3.5}
+
+cube_dict = dict()
+for num in range(1,20):
+    cube_dict[num] = num ** 3
+print("Cube roots dict: ", cube_dict)
+
+cubed = {num : num ** 3 for num in range(0,20)}
+print("COnsize declaration: ",cubed)
+
+new_price = {item : value*0.76 for (item, value) in old_price.items()}
+print("Updated Price: ", new_price)
+
+person_data = {'John': 36, 'micheal': 25, 'Russom': 45, 'hap':58}
+even_age = {k:v for (k,v) in person_data.items() if v%2==0}
+print("Even aged persons: ", even_age)
+
+even = {k:v for (k,v) in person_data.items() if v%2==0 if v < 40}
+print("Even aged persons whose age less than 40: ", even)
+
+y_o = {k : ('old' if v>40 else 'young') for (k,v) in person_data.items()}
+print(y_o)
+
+dic1 = dict()
+for k1 in range(1,10):
+    dic1[k1] = dict()
+    for k2 in range(1,5):
+        dic1[k1][k2] = k1*k2
+
+
+dic = {
+    k1 : {k2 : k1*k2 for k2 in range(1,5)} for k1 in range(1,10)
+}
+print(dic)
