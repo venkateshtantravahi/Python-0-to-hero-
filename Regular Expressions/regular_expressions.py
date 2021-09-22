d3d99f6665d15139758b49777f56119359d0e549
venkatesh <venkateshtantravahi99@gmail.com>
2021-09-22 19:25:44 +0530

    new commit

---
+++ Regular Expressions/regular_expressions.py
@@ -0,0 +1,23 @@
+"""
+A regular expression(REgEx) is a sequence f characters that defines a search pattern
+MetaCHaracters : [] . ^ $ * ? {} () \ | 
+[] -> set of characters
+. -> any single character except '\n'
+^ -> checks if string starts with certain pattern or character
+$ -> checks if strings ends with certain character
+* -> used for counting occurances of patterns
++, ? -> matches one or more occurances towards the pattern left
+{x,y}, [x-y] -> its used for matching number of characters where x is lower limit and y is upper limit
+| -> it acts like a or operator
+() ->grouping in pattern 
+"""
+import re 
+
+pattern = '^a...s$'
+test_string = 'abyss'
+result = re.match(pattern=pattern, string=test_string)
+
+if result:
+    print("Search Successful")
+else:
+    print("Search unsuccessful")
